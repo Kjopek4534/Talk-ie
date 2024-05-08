a49d1c1005be4c84e872a8ec9a0adba881477858
Konrad <deziemniaczek@gmail.com>
2024-05-08 11:39:38 +0200

    feat(login): Added findOne method to userService to find user by username in database

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+export type User = any
+
+const prisma = new PrismaClient()
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async findOne(username: string): Promise<User | undefined> {
+    return prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    })
+  }
+}
