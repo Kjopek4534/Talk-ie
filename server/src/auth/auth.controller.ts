b7cee185fc00e4ddc38785beaf56b706a0c00d53
Konrad <deziemniaczek@gmail.com>
2024-05-08 18:13:40 +0200

    feat(authGuard): added authGuard to check for valid JWT token and added example route that uses authGuard

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common'
+import { AuthGuard } from './auth.guard'
 import { AuthService } from './auth.service'
 
 @Controller('auth')
@@ -9,5 +19,11 @@ export class AuthController {
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password)
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user
   }
 }
