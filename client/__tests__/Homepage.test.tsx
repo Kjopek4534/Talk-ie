9d9b28faf52df77f97b40b12015b43b2e587cc1c
Illirock <patryk.mendelewski@gmail.com>
2024-06-04 19:34:45 +0200

    feat(test): Adding basic tests for HomePage

---
+++ client/__tests__/Homepage.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import HomePage from '@/app/page'
+
+describe('SignUp', () => {
+  it('renders the home page', () => {
+    render(<HomePage />) //Arrange
+
+    const myElement = screen.getByText(/Sign Up/i) //Act
+    expect(myElement).toBeInTheDocument() //Assert
+  })
+})
